fe9e79e5346b3e48f86349426ac4dcbafb756366
lutherv01 <aldirafikaluthfip@gmail.com>
2025-07-05 17:20:09 +0700

    tes

--- src/app/kontak/page.jsx
+++ src/app/kontak/page.jsx
@@ -69,7 +69,16 @@ const KontakPage = () => {
 
                 {/* Google Maps */}
                 <div className="w-full">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.338981620444!2d107.6255820757401!3d-6.969276668241611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e9bc3974981d%3A0x613eec0feec9fcf7!2sTelkom%20University%20Landmark%20Tower%20(TULT)!5e0!3m2!1sen!2sid!4v1751709766801!5m2!1sen!2sid" width="600" height="450" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.338981620444!2d107.6255820757401!3d-6.969276668241611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e9bc3974981d%3A0x613eec0feec9fcf7!2sTelkom%20University%20Landmark%20Tower%20(TULT)!5e0!3m2!1sen!2sid!4v1751710365036!5m2!1sen!2sid"
+                    width="100%"
+                    height="400"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    className="rounded-lg max-sm:h-40"
+                  />
                 </div>
 
                 {/* Info Kontak */}
